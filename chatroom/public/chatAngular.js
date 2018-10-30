dcc15e268690d6c81b48383193f109012b927f02
stStembridge <ststembridge@gmail.com>
2018-10-30 23:59:47 +0000

    index.js should be done

--- chatroom/public/chatAngular.js
+++ chatroom/public/chatAngular.js
@@ -1,42 +1,43 @@
 var chatApp = angular.module('chatApp', []);
 chatApp.controller('chatCtrl', function($scope, $http) {
     $scope.three = 3;
-   // $scope.time="9:00 am";
-   $scope.messageContent = "";
-    $scope.messages = [
-    {
-        content: "Message 1",
-        time: "9:00 am"
-    },
-    {
-        content: "Message 2",
-        time: "10:30 am"
-    }
+    // $scope.time="9:00 am";
+    $scope.messageContent = "";
+    $scope.messages = [{
+            content: "Message 1",
+            time: "9:00 am"
+        },
+        {
+            content: "Message 2",
+            time: "10:30 am"
+        }
     ];
-    
+
     $scope.addMessage = function($event) {
+        var d = new Date();
+        var currentTime = d.toLocaleTimeString();
         $scope.messages.push({
             content: $scope.messageContent,
-            time: "2:26pm"
+            time: currentTime
         })
     }
 });
 
 chatApp.directive('chatMessage', function() {
-   return {
-       scope: {
-           message: "="
-       },
-       restrict: "E",
-       templateUrl: "chatMessage.html"
-   } 
+    return {
+        scope: {
+            message: "="
+        },
+        restrict: "E",
+        templateUrl: "chatMessage.html"
+    }
 });
 chatApp.directive('userDisplay', function() {
-   return {
-       scope: {
-           user: "="
-       },
-       restrict: "E",
+    return {
+        scope: {
+            user: "="
+        },
+        restrict: "E",
         templateUrl: "userDisplay.html"
-   };
+    };
 });
--- chatroom/routes/index.js
+++ chatroom/routes/index.js
@@ -2,11 +2,35 @@ var express = require('express');
 var router = express.Router();
 var app = express();
 var path = require('path');
+//var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
+  console.log('entered router.get function')
   res.sendFile(path.join(__dirname, '../public', 'chatroom.html'));
+});
+
+var messages = [{
+            content: "Message 1",
+            time: "9:00 am"
+        },
+        {
+            content: "Message 2",
+            time: "10:30 am"
+        }
+];
+
+router.get('/messages', function(req, res) {
+  console.log("In Messages");
+  res.send(messages);
+});
+
+router.post('/messages', function(req, res) {
+   console.log("In message Post");
+    console.log(req.body);
+    messages.push(req.body);
+    res.end('{"success" : "Updated Successfully", "status" : 200}');
 });
 
 module.exports = router;
